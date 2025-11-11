8c1b2613f67c5d377f94ae4016b026024e8feb78
mattsu <matsui.atsushi.home@gmail.com>
2025-11-11 19:34:58 +0900

    chore(numfmt): update spell-checker config for undelimited format
    Add "undelimited" to ignored spell-checker terms to prevent false
    positives when working with numfmt formatting logic.

--- src/uu/numfmt/src/format.rs
+++ src/uu/numfmt/src/format.rs
@@ -2,7 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// spell-checker:ignore powf
+// spell-checker:ignore powf undelimited
 use lexical_core::parse_partial;
 use uucore::display::Quotable;
 use uucore::translate;
